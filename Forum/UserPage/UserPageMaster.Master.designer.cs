c1d2033aed60c2765206e5b7df8dfa42e4a278cb
Vicky Banh <samsokhua1@gmail.com>
2019-07-26 13:33:42 -0400

    styled scrollbar for bookmark page

--- Forum/UserPage/UserPageMaster.Master.designer.cs
+++ Forum/UserPage/UserPageMaster.Master.designer.cs
@@ -22,6 +22,15 @@ namespace Forum.UserPage {
         protected global::System.Web.UI.WebControls.ContentPlaceHolder head;
         
         /// <summary>
+        /// Header control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::Forum.UserPage.Header Header;
+        
+        /// <summary>
         /// form1 control.
         /// </summary>
         /// <remarks>
