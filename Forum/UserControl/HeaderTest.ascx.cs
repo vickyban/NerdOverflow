8bd6ee1d6f5a67305f5b70c99af44f3b80f7b66b
Vicky Banh <samsokhua1@gmail.com>
2019-07-25 21:49:10 -0400

    Added models

--- Forum/UserControl/HeaderTest.ascx.cs
+++ Forum/UserControl/HeaderTest.ascx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Forum.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,6 +10,7 @@ namespace Forum.UserControl
 {
     public partial class HeaderTest : System.Web.UI.UserControl
     {
+        public Post Post { get; set; }
         protected void Page_Load(object sender, EventArgs e)
         {
 
