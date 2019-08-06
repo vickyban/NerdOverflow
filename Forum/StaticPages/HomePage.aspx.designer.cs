17ee006ed7918ed8f3e6d485d99b12ac6ab6ff85
Vicky Banh <samsokhua1@gmail.com>
2019-08-06 10:04:41 -0400

    Add footer control

--- Forum/StaticPages/HomePage.aspx.designer.cs
+++ Forum/StaticPages/HomePage.aspx.designer.cs
@@ -38,5 +38,14 @@ namespace Forum.StaticPages {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Image imgShowcase;
+        
+        /// <summary>
+        /// Footer control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::Forum.UserControl.Footer Footer;
     }
 }
